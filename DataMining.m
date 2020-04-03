efb0a5cb800cf3ec80fbbf9238f6e253740762da
Rezky Putratama Raharjo <49394196+ekky2412@users.noreply.github.com>
2020-04-03 16:35:18 +0700

    Add files via upload

---
+++ DataMining.m
@@ -0,0 +1,5 @@
+training = [3 1 2 1;2 2 1 3;3 1 1 2;3 2 2 3;3 2 2 3;2 2 1 3;1 2 1 2;3 2 1 2;3 2 2 3;1 1 1 2;2 2 2 2;3 2 2 1;2 2 2 3;3 2 2 3;3 2 1 3;2 1 1 2;2 1 2 3;2 1 2 1;1 2 2 3;1 2 1 2];
+group = ["Tidak";"Ya";"Tidak";"Ya";"Ya";"Ya";"Tidak";"Ya";"Ya";"Tidak";"Tidak";"Ya";"Ya";"Ya";"Ya";"Tidak";"Ya";"Tidak";"Ya";"Tidak"];
+sampel = [1 2 2 3;2 2 1 3;3 1 1 1;1 1 1 3;1 2 1 3];
+model = fitcknn(training,group,'NumNeighbors',3);
+disp(predict(model,sampel));
